5308974ac3e0fdb2cd4444bfca49c9d81e33cd35
mjsherwood <mjsherwood@gmail.com>
2023-09-22 22:00:03 -0700

    week 3 update with put, post, delete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -23,4 +23,77 @@ const getSingle = asyncHandler(async (req, res, next) => {
     res.status(200).json(lists[0]);
 });
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, favoriteColor, birthday } = req.body.contact;
+
+    if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const result = await mongodb.getDb().db().collection('contacts').insertOne({
+      contact: {
+        firstName, 
+        lastName, 
+        email, 
+        favoriteColor, 
+        birthday
+      }
+    });
+
+    res.status(201).json({ id: result.insertedId });
+  } catch (error) {
+    console.error('Error creating contact:', error);
+    next(error); 
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).send('Invalid ObjectId format');
+    }
+    
+    const contactId = new ObjectId(req.params.id);
+
+    const updatedContact = {
+      contact: req.body.contact
+    };
+
+    const result = await mongodb.getDb().db().collection('contacts').updateOne({ _id: contactId }, { $set: updatedContact });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).send('Contact not found');
+    }
+
+    res.status(204).end();
+  } catch (error) {
+    console.error(`Error updating contact with id ${req.params.id}:`, error);
+    next(new Error('Server error while updating the contact'));
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).send('Invalid ObjectId format');
+    }
+
+    const contactId = new ObjectId(req.params.id);
+
+    const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: contactId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).send('Contact not found');
+    }
+
+    // Respond with 200 OK to signify successful deletion with a message
+    res.status(200).send('Contact deleted successfully');
+
+  } catch (error) {
+    console.error(`Error deleting contact with id ${req.params.id}:`, error);
+    next(new Error('Server error while deleting the contact'));
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };

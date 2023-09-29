f88d979bf22cca720bcaa07e338e956f8d8ed34c
mjsherwood <mjsherwood@gmail.com>
2023-09-28 21:06:43 -0700

    Deleted unneeded files

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const michaelRoute = (req, res) => {
+    res.send('Michael Sherwood');
+};
+
+const karleannaRoute = (req, res) => {
+    res.send('Karleanna Sherwood');
+};
+
+module.exports = {
+    michaelRoute,
+    karleannaRoute
+}
